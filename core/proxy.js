f3549b719a7aae899032c76dc907067f185342da
formula1 <samtobia@gmail.com>
2014-11-29 19:02:22 -0800

    env vars

--- core/proxy.js
+++ core/proxy.js
@@ -1,7 +1,11 @@
 global.__root = __dirname+"/..";
 
+var hp = process.env.hp || 3001;
+var wp = process.env.wp || 9998;
+var sp = process.env.sp || 3500;
+
 
 var ProxyServer = require(__dirname+"/network/Proxy2Client_com.js");
 
 
-var proxy = new ProxyServer(3001,9998,3500);
+var proxy = new ProxyServer(hp,wp,sp);
