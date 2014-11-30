1451158e44c83ccac65a332e96118075faf95cd6
formula1 <samtobia@gmail.com>
2014-11-29 20:18:19 -0800

    multi cookie :C

--- core/network/Proxy2Client_com.js
+++ core/network/Proxy2Client_com.js
@@ -129,8 +129,7 @@ ProxyServer.prototype.handleSocketMessage = function(message,c){
       console.log("client ok "+message.client);
       message.data = message.client;
       this.clients[message.client].res.writeHead(200,{
-       'Set-Cookie': 'client='+message.client+"; pwp="+wp+"; path=/; expires=01 Jan 2020 00:00:00 GMT"
-
+       'Set-Cookie': 'client='+message.client+"; pwp="+wp+"; path=/; expires=01 Jan 2020 00:00:00 GMT;"
       });
       var fileStream = fs.createReadStream(__dirname+"/public/redirect.html");
       fileStream.pipe(this.clients[message.client].res);
@@ -193,7 +192,7 @@ ProxyServer.prototype.shield = function(req,res){
   if(u.pathname === "/exit"){
     res.writeHead(302, {
       'Location': '/',
-      "Set-Cookie": "client=0;pwp=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+      "Set-Cookie": "client=0; pwp=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;"
 
       //add other headers here...
     });
@@ -203,7 +202,7 @@ ProxyServer.prototype.shield = function(req,res){
   var headers = {};
   if(cook.client){
     if(!(cook.client in this.clients)){
-      headers["Set-Cookie"]= "client=0;pwp=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+      headers["Set-Cookie"]= "client=0; pwp=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
       delete cook.client;
     }else{
       return;
